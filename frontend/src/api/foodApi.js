f9c5999e03564e4e400cb0a61dc33f0a205fc80f
thinhvpg2410 <thinh.vpg@gmail.com>
2026-04-08 19:23:53 +0700

    feat(Ui/API): connect api food

--- frontend/src/api/foodApi.js
+++ frontend/src/api/foodApi.js
@@ -5,3 +5,6 @@ const client = createHttpClient(apiConfig.foodServiceBaseUrl)
 const unwrap = (response) => response.data
 
 export const getFoods = () => client.get('/foods').then(unwrap)
+export const createFood = (payload) => client.post('/foods', payload).then(unwrap)
+export const updateFood = (id, payload) => client.put(`/foods/${id}`, payload).then(unwrap)
+export const deleteFood = (id) => client.delete(`/foods/${id}`).then(unwrap)
